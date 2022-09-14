96409d0884bcd56ff4b246389d0181155e783678
YingVickyCao <ying.vicky.cao@outlook.com>
2022-09-14 20:58:24 +0800

    ...

--- app/src/test/java/com/example/hades/android/gradleconfigcode/ExampleUnitTest.java
+++ app/src/test/java/com/example/hades/android/gradleconfigcode/ExampleUnitTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.io.InputStream;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -13,7 +15,25 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
-
         System.out.println(new Verify().getNum());
+
+//        // /Users/account/Documents/project/AndroidGradleConfig/app/build/intermediates/java_res/procDebugUnitTest/out/a.txt
+//        String rootPath = getClass().getResource("/a.txt").getFile().toString();
+//        System.out.println(rootPath);
+//
+//
+//        // file:/Users/account/Documents/project/AndroidGradleConfig/app/build/intermediates/java_res/procDebugUnitTest/out/a.txt
+//        String rootPath2 = getClass().getResource("/a.txt").toString();
+//        System.out.println(rootPath2);
+//
+//        // file:/Users/account/Documents/project/AndroidGradleConfig/app/build/intermediates/javac/procDebugUnitTest/classes/
+//        String rootPath3 = getClass().getResource("/").toString();
+//        System.out.println(rootPath3);
+//
+//        // file:/Users/account/Documents/project/AndroidGradleConfig/app/build/intermediates/javac/procDebugUnitTest/classes/com/example/hades/android/gradleconfigcode/
+//        String rootPath4 = getClass().getResource("").toString();
+//        System.out.println(rootPath4);
+
+//        InputStream inputStream = getClass().getResourceAsStream("a.txt");
     }
 }
